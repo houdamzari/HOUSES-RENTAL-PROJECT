7118427cd5ad5f7b458a56f4c430e5b5d4e5164f
ismail bayoussef <bayoussefismail@gmail.com>
2021-11-18 11:38:15 +0000

    position

---
+++ src/Utilities/useCurrentPosition.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export function useCurrentPosition(options) {
+  const [position, setPosition] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    let canceled = false;
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        if (!canceled) {
+          setPosition(position);
+        }
+      },
+      (error) => {
+        if (!canceled) {
+          setError(error);
+        }
+      },
+      options
+    );
+
+    return () => {
+      canceled = true;
+    };
+  }, [options]);
+
+  return [position, error];
+}
