4a07f25d35c7a83a66f85d432f815972d6d8feea
ismail bayoussef <bayoussefismail@gmail.com>
2021-11-18 11:31:01 +0000

    .

--- src/Profile/BigContainer.js
+++ src/Profile/BigContainer.js
@@ -1,76 +1,22 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { theme } from "../Utilities/theme";
-import Button from "../PostPage/Button";
-import Grid from "../PostPage/Grid";
-import Heading from "../PostPage/Heading";
-import Description from "../PostPage/Description";
-import Spacer from "../Utilities/Spacer";
-import Comodities from "../PostPage/Comodities";
-import Requirements from "../PostPage/Requirements";
+import PictureSection from "./PictureSection";
+import Heading from "./Heading";
 
 const Container = styled.div`
   display: flex;
   flex-direction: row;
   gap: 10rem;
-  position: relative;
-  top: 3rem;
-  left: 1rem;
-  overflow-x: hidden;
 `;
 function BigContainer(props) {
   return (
     <Container>
-      <div>
-        <Grid />
-      </div>
-      <div>
-        <Heading title="Modern apartment" name price />
-        <Spacer margin="2rem" />
-        <Description />
-        <Spacer margin="4rem" />
-
-        <Heading title="HOUSE COMMODITIES" />
-        <Spacer margin="2rem" />
-        <Comodities />
-        <Spacer margin="4rem" />
-
-        <Heading title="Requirements " />
-        <Spacer margin="2rem" />
-        <Requirements />
-        <Spacer margin="3rem" />
-
-        <Button />
-      </div>
+      <div></div>
+      <PictureSection />
+      <Heading />
     </Container>
   );
 }
 
 export default BigContainer;
-=======
-import React from 'react';
-import styled from "styled-components";
-import {theme} from "../Utilities/theme";
-import PictureSection from "./PictureSection";
-import Heading from "./Heading";
-
-const Container = styled.div`
-    
-    display: flex;
-  flex-direction: row;
-  gap: 10rem;
-    
-`
-function BigContainer(props) {
-    return (
-        <Container>
-            <div></div>
-            <PictureSection/>
-            <Heading/>
-        </Container>
-    );
-}
-
-export default BigContainer;
->>>>>>> f41d5adbed3cb63a4248feac020bf0c075be71cc
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import styled from "styled-components";
 import { theme } from "../Utilities/theme";
@@ -14,24 +13,3 @@ function Profile(props) {
 }
 
 export default Profile;
-=======
-import React from 'react';
-import styled from "styled-components";
-import {theme} from "../Utilities/theme";
-import BigContainer from "./BigContainer";
-
-const Container = styled.div`
-
-    
-`
-function Profile(props) {
-    return (
-        <Container>
-                <BigContainer/>
-
-        </Container>
-    );
-}
-
-export default Profile;
->>>>>>> f41d5adbed3cb63a4248feac020bf0c075be71cc
