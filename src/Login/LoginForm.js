d0c663ec8ac8495e74874145c4f69da3ae7e9cac
ismail bayoussef <bayoussefismail@gmail.com>
2021-11-18 16:03:34 +0000

    .

--- src/Login/LoginForm.js
+++ src/Login/LoginForm.js
@@ -1,47 +1,69 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import {theme} from "../Utilities/theme";
+import { theme } from "../Utilities/theme";
 import logo from "./media/LOGO.png";
+import { useHistory } from "react-router-dom";
 import Button from "../Login/Button";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import { users } from "../data";
 const Container = styled.div`
-
   position: relative;
   left: 8rem;
-    .formcontrol{
-        width: 22.7rem;
-        height: 2.2rem;
-        margin-top: 1rem;
-        border-radius: .5rem;
-        background-color:rgba(2, 102, 112,0.1);
-      
-}
-      h4{
-          position: relative;
-        font-weight: 300; 
-        
-      }
-      img{
-        height: 10rem;
-        position: relative;   
-      }
-`
+  .formcontrol {
+    width: 22.7rem;
+    height: 2.2rem;
+    margin-top: 1rem;
+    border-radius: 0.5rem;
+    background-color: rgba(2, 102, 112, 0.1);
+  }
+  h4 {
+    position: relative;
+    font-weight: 300;
+  }
+  img {
+    height: 10rem;
+    position: relative;
+  }
+`;
 function LoginForm(props) {
-    return (
-        <Container>
-            <img src={logo} alt="logo"/>
-            <h4 className="formlabel">Email :</h4>
-            <div>
-                <input type="email" className="formcontrol" id="inputPassword"/>
-            </div>
-            <h4 className="formlabel">Password :</h4>
-            <div>
-                <input type="password" className="formcontrol" id="inputPassword"/>
-            </div>
-             <Button />
-        </Container>
-    );
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  let history = useHistory();
+  const handleSubmit = () => {
+    users.map((p) => {
+      if (p.email === email && p.password === password) {
+        window.localStorage.setItem("login", true);
+        history.push("/");
+      }
+    });
+    if (window.localStorage.getItem("login") === "false") {
+      alert("Wrong credentials");
+    }
+  };
+  return (
+    <Container>
+      <img src={logo} alt="logo" />
+      <h4 className="formlabel">Email :</h4>
+      <div>
+        <input
+          type="email"
+          className="formcontrol"
+          onChange={(e) => setEmail(e.target.value)}
+          id="inputPassword"
+        />
+      </div>
+      <h4 className="formlabel">Password :</h4>
+      <div>
+        <input
+          type="password"
+          className="formcontrol"
+          onChange={(e) => setPassword(e.target.value)}
+          id="inputPassword"
+        />
+      </div>
+      <Button handleSubmit={handleSubmit} />
+    </Container>
+  );
 }
 
 export default LoginForm;
